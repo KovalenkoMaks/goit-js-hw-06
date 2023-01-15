b49e4944f4a79302b0071937169799145a62a3a6
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-15 18:34:56 +0200

    add task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsEl = ingredients.map((ingredient) => {
+  const elem = document.createElement("li");
+  elem.textContent = ingredient;
+  elem.className = "item";
+  return elem;
+});
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.append(...ingredientsEl);
