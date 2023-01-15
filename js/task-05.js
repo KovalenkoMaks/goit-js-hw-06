924557c9d0c1df1cc83ffaf5d48787b4766a681c
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-15 22:35:29 +0200

    add task-05

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,7 @@ const refs = {
   inputEl: document.querySelector("#name-input"),
   userName: document.querySelector("#name-output"),
 };
+
 refs.inputEl.addEventListener("input", (event) => {
   let value = event.target.value;
   if (value.trim() === "") {
