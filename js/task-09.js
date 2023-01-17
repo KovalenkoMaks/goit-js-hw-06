54fea86a9f9ecb26e19c7754823eebcf589aad32
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-17 18:34:54 +0200

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+console.log(colorValue);
+
+changeColorBtn.addEventListener("click", (event) => {
+  colorValue.textContent = getRandomHexColor();
+  body.style.backgroundColor = colorValue.textContent;
+});
