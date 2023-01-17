db88f8e0f4b910301f55154df3cf5cdfd42a5663
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-17 23:13:11 +0200

    add task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
+const refs = {
+  input: document.querySelector("input"),
+  inputMin: document.querySelector("input").min,
+  inputMax: document.querySelector("input").max,
+  createBtn: document.querySelector("button[data-create]"),
+  destroyBtn: document.querySelector("button[data-destroy]"),
+  boxes: document.getElementById("boxes"),
+};
+
+refs.createBtn.addEventListener("click", () => {
+  createBoxes(refs.input.value);
+});
+
+refs.destroyBtn.addEventListener("click", () => {
+  while (refs.boxes.firstChild) {
+    refs.boxes.removeChild(refs.boxes.firstChild);
+  }
+});
+
+function createBoxes(amount) {
+  const div = [];
+  for (let index = 0; index < amount; index += 1) {
+    let divEl = document.createElement("div");
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.style.height = `${30 + index * 10}px`;
+    divEl.style.width = `${30 + index * 10}px`;
+    div.push(divEl);
+  }
+  refs.boxes.append(...div);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
