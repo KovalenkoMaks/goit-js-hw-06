8b27a6ad568b7bcfdec420c35b58256bad4c9c23
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-01-17 17:59:53 +0200

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
-
+const refs = {
+  formEl: document.querySelector(".login-form"),
+  emailInput: document.querySelector(".login-form").firstElementChild,
+  passInput:
+    document.querySelector(".login-form").firstElementChild.nextElementSibling,
+};
+refs.formEl.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    !event.currentTarget.elements[0].value ||
+    !event.currentTarget.elements[1].value
+  ) {
+    return alert("Please, enter your email and password!");
+  }
+  const formData = {
+    email: event.currentTarget.elements[0].value,
+    password: event.currentTarget.elements[1].value,
+  };
+  console.log(formData);
+  event.currentTarget.reset();
+}
